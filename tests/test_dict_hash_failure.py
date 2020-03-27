01e304f8a51cdc37f8f700e68a9f304a10887e36
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-27 15:34:06 +0100

    Extended test coverage

---
+++ tests/test_dict_hash_failure.py
@@ -0,0 +1,8 @@
+from dict_hash import sha256, dict_hash
+import pytest
+
+def test_dict_hash_failure():
+    with pytest.raises(ValueError):
+        dict_hash({
+            "Test": lambda x: 0
+        })
